9567f5ca7a416b8679f190174d24de5073a81615
Daniel C. Ribeiro <dcoutinho.96@gmail.com>
2025-08-20 05:43:13 -0300

    fix: use postcss to fix style not being loaded

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import tailwindcss from "@tailwindcss/vite";
 
 const escapeRegExp = (s: string) => s.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
 
@@ -16,7 +15,6 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
-      tailwindcss(),
       VitePWA({
         registerType: "autoUpdate",
         includeAssets: [
@@ -89,8 +87,8 @@ export default defineConfig(({ mode }) => {
         include: ["src/**/*.{ts,tsx}"],
 
         exclude: [
-          "src/main.tsx",            
-          "src/gql/generated.ts",    
+          "src/main.tsx",
+          "src/gql/generated.ts",
           "**/*.d.ts"
         ],
 
