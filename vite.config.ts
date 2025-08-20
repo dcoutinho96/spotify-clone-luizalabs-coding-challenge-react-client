8315b28ebfeb09d3ca16400107a8679a72216e25
Daniel C. Ribeiro <dcoutinho.96@gmail.com>
2025-08-20 01:15:48 -0300

    feat: add sonarqube

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,9 @@ export default defineConfig({
       "dist",
       "e2e/**", 
     ],
+    coverage: {
+      reporter: ["text", "lcov"],   
+      reportsDirectory: "coverage",
+    },
   },
 });
