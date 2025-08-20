04e390c7704225b913e817d4730b3640492e54e4
Daniel C. Ribeiro <dcoutinho.96@gmail.com>
2025-08-19 23:52:39 -0300

    fix: fix e2e tests

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./test/setup.ts"
+    setupFiles: "./test/setup.ts",
+    exclude: [
+      "node_modules",
+      "dist",
+      "e2e/**", 
+    ],
   },
 });
