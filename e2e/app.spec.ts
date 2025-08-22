91f59c569dd32cade190a32c6e84d0948be1b1d9
Daniel C. Ribeiro <dcoutinho.96@gmail.com>
2025-08-22 00:33:58 -0300

    fix: remove e2e test for now

--- e2e/app.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('hello query works', async ({ page }) => {
-  await page.goto('/');
-  await expect(page.getByText(/Message:/)).toBeVisible();
-})
